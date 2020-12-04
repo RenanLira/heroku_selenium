5933f6fa9fe19c0dcb38179d9c79b983b74817b7
RenanLira <48999721+RenanLira@users.noreply.github.com>
2020-12-04 14:45:49 -0300

    novo_teste

--- app.py
+++ app.py
@@ -1,30 +1,16 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from flask import *  
+from script import runScript
+app = Flask(__name__)  
+  
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.action_chains import ActionChains
+@app.route('/script', methods = ['POST'])  
+def login():  
+      keyWord=request.form['uname']
+      ans = runScript(keyWord)
+      return '<div>' +ans+ '</div>' 
 
-import time
-import os
-
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-
-def main():
-    
-
-    driver.get('https://google.com.br/')
-
-    print(driver.page_source)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__':  
+   app.run(debug = True) 
